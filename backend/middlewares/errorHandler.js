35ca6b943ed88d5502f553f8dc12d6dbfe09be10
Sibtain Raza <khanbhai14063@gmail.com>
2026-09-23 02:19:42 +0530

    Authentication part has been done , like Register, login, protect, updateProfile, authRoutes

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -4,7 +4,6 @@ export const notFound = (req, res, next) =>{
 
 export const errorHandler = (err, req, res, next) =>{
     console.log(err)
-
     const status = res.statusCode && res.statusCode != 200 ? res.statusCode : 500;
     res.status(status).json({
         message: err.message || "Server error",
