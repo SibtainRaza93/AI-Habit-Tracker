cc82a8cedca324c9c50d165032a6276975fef826
Sibtain Raza <khanbhai14063@gmail.com>
2026-09-24 23:42:52 +0530

    HabitController updated with , createHabit, updatehabit, DeleteHbait

---
+++ backend/controllers/habit.controller.js
@@ -0,0 +1,94 @@
+import Habit from "../models/habit.models.js";
+import HabitLog from "../models/habitLog.models.js";
+
+export const getHabits = async (req, res) => {
+    try {
+        const { includeArchived } = req.query;
+        const filter = { userId: req.user._id };
+        if (includeArchived != "true") filter.isArchived = false;
+        const habits = await Habit.find(filter).sort({ oerder: 1, createdAt: 1 });
+        res.json(habits);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+
+export const createHabit = async (req, res) => {
+    try {
+        const {
+            name,
+            dexcription,
+            category,
+            frequency,
+            targetDays,
+            color,
+            icon,
+        } = req.body;
+
+        if (!name) return res.status(400).json({ message: "habits name is required" });
+
+        const count = await Habit.countDocuments({ userId: req.user._id });
+        const habit = await Habit.create({
+            userId: req.user._id,
+            name,
+            dexcription,
+            category,
+            frequency,
+            targetDays,
+            color,
+            icon,
+            order: count,
+        });
+        res.status(201).json(habit);
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    };
+};
+
+export const updateHabit = async (req, res)=>{
+    try {
+        const habit = await Habit.findOne({
+            _id: req.params.id,
+            userId: req.user._id,
+        });
+
+        if(!habit) return res.status(404).json({message: "Habit not found"});
+
+        const fields = [
+            "name",
+            "dexcription",
+            "category",
+            "frequency",
+            "targetDays",
+            "color",
+            "icon",
+            "order",
+        ];
+        for(const f of fields){
+            if(req.body[f] !== undefined) habit[f] = req.body[f];
+        };
+        await habit.save();
+        res.json(habit);
+
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    };
+};
+
+//Delete habits functions below
+
+export const deleteHabit = async (treq, res)=>{
+    try {
+        const habit = await Habit.findOneAndDelete({
+            _id: req.params.id,
+            userId: res.user._id,
+        });
+        
+        if(!habit) return res.status(404).json({message: "Habit is not found"});
+        await HabitLog.deleteMany({ habitId: habit._id, userId: req.user._id});
+        res.json({message: "Habit Deleted"});
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+}
